66eb111c6fdef1adf88c42e6d762c1fb383ebbf7
cnieblas16 <cinieblas16@gmail.com>
2025-11-20 18:55:33 -0600

    Matplot

--- .venv/src/plot.py
+++ .venv/src/plot.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Data for plotting
+# Matplotlib es una biblioteca de código
+# abierto para Python que se utiliza para
+# crear visualizaciones de datos, como gráficos y diagramas.
+
+# Datos para graficar
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([2, 4, 6, 8, 10])
 
-# Create the plot
+# Crear plot
 plt.plot(x, y)
 
-# Add labels and title
+# Ejex y titulo
 plt.xlabel("Eje x")
 plt.ylabel("Eje y")
 plt.title("Recta")
